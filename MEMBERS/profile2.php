ff68b837edc4d4e4e88660258206cda3cff8a45c
neozeph <152301596+neozeph@users.noreply.github.com>
2025-05-26 04:35:27 +0800

    Update profile2.php

--- MEMBERS/profile2.php
+++ MEMBERS/profile2.php
@@ -17,11 +17,6 @@ try {
     die("Database connection failed: " . $e->getMessage());
 }
 
-// Check if user is logged in
-if (!isset($_SESSION['email'])) {
-    header("Location: login.php");
-    exit();
-}
 
 $userEmail = $_SESSION['email'];
 // Fetch user data
