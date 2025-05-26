ffe07aadbfb51834910b80917ba22f5678f60403
neozeph <152301596+neozeph@users.noreply.github.com>
2025-05-26 16:08:28 +0800

    logout

--- MEMBERS/pollDashboard2.php
+++ MEMBERS/pollDashboard2.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="icon" href="/images/logo.png">
-    <title>Admin Dashboard</title>
+    <title>LycanCore Dashboard</title>
     <style>
         * {
             box-sizing: border-box;
--- login-branch/logout.php
+++ login-branch/logout.php
@@ -12,6 +12,6 @@ if (ini_get("session.use_cookies")) {
     );
 }
 
-header("Location: /index.html");
+echo '<script>top.location.href = "/index.html";</script>';
 exit();
 ?>
