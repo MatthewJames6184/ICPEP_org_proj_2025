892ba83f433e960018f9671666f56a39f965f470
Jamuzzz <ariasjamesmatthew@gmail.com>
2026-06-17 21:20:13 +0800

    Update send_otp.php

--- mailing/send_otp.php
+++ mailing/send_otp.php
@@ -23,8 +23,8 @@ try {
     $mail->isSMTP();
     $mail->Host       = 'smtp.gmail.com';
     $mail->SMTPAuth   = true;
-    $mail->Username   = 'arias.james18@gmail.com'; // Your Gmail
-    $mail->Password   = 'outtnigvgwkanrgp';    // Your Gmail App Password
+    $mail->Username   = ''; // Your Gmail
+    $mail->Password   = '';    // Your Gmail App Password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
     $mail->Port       = 587;
 
